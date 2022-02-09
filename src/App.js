4766827f46abfc0e02f8e506358a48aa92a4096e
Hitogiri <obieze.isid@gmail.com>
2022-02-08 19:36:22 -0600

    Updated app to include a shop page, a collection preview component, and a collection item component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
-import HomePage from './pages/homepage/HomePage.component';
-import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import HomePage from './pages/homepage/HomePage.component';
+import ShopPage from './pages/shop/Shop.component';
+import './App.css';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
