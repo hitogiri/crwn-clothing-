5f0b2412f2e5f33d55594e82dd9ad2465bf4c18a
Hitogiri <obieze.isid@gmail.com>
2022-02-08 20:07:32 -0600

    Now have a header component with links to the home and shop pages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {Switch, Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/HomePage.component';
 import ShopPage from './pages/shop/Shop.component';
+import Header from './components/header/Header.component';
 import './App.css';
 
 function App() {
   return (
     <div>
+    <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
