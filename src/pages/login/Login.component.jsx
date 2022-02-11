bde88d6e56e6baf47a56883f0b9e538b039c623d
Hitogiri <obieze.isid@gmail.com>
2022-02-10 21:18:00 -0600

    implemented firebase utils, included a way to store authenticated users into database

--- src/pages/login/Login.component.jsx
+++ src/pages/login/Login.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/SignIn.component';
+import SignUp from '../../components/sign-up/SignUp.component';
 
 import './login.style.scss';
 
 const Login = () => (
   <div className='login'>
     <SignIn />
+    <SignUp />
   </div>
 )
 
